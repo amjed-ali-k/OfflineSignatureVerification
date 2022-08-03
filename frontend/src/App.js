da7f45e1fe88a28df1b6493b2ba7de36cc7e82c2
amjed-ali-k <amjedmgm@gmail.com>
2022-08-03 20:18:20 +0400

    Changed limit to 4

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,18 +1,25 @@
-import React, {useState} from 'react';
-import UploadImageContainer from './Containers/UploadImageContainer/UploadImageContainer'
-import VerifyImageContainer from './Containers/VerifyImageContainer/VerifyImageContainer'
+import React, { useState } from "react";
+import UploadImageContainer from "./Containers/UploadImageContainer/UploadImageContainer";
+import VerifyImageContainer from "./Containers/VerifyImageContainer/VerifyImageContainer";
 
-import './App.css';
+import "./App.css";
 
 function App() {
-
-
   return (
     <div className="App">
-        <h1>Signature Verification web demo</h1>
-        <UploadImageContainer imgLimit="3" submitButtonText="Upload" headingText="Upload 3 new signatures of the customer" allowMultiple={true}/>
-        <VerifyImageContainer imgLimit="1" submitButtonText="Verify" headingText="Upload a new signature of the customer to verify" allowMultiple={false}/>
-
+      <h1>Signature Verification web demo</h1>
+      <UploadImageContainer
+        imgLimit="4"
+        submitButtonText="Upload"
+        headingText="Upload 3 new signatures of the customer"
+        allowMultiple={true}
+      />
+      <VerifyImageContainer
+        imgLimit="1"
+        submitButtonText="Verify"
+        headingText="Upload a new signature of the customer to verify"
+        allowMultiple={false}
+      />
     </div>
   );
 }
--- main.py
+++ main.py
@@ -9,17 +9,24 @@ import json
 import math
 
 
-app = Flask(__name__, static_folder='./frontend/build/static', template_folder='./frontend/build')
+app = Flask(
+    __name__,
+    static_folder="./frontend/build/static",
+    template_folder="./frontend/build",
+)
+
 
 def load_model():
-    device = torch.device('cpu')
+    device = torch.device("cpu")
     model = SiameseConvNet().eval()
-    model.load_state_dict(torch.load('Models/model_large_epoch_20', map_location=device))
+    model.load_state_dict(
+        torch.load("Models/model_large_epoch_20", map_location=device)
+    )
     return model
 
 
 def connect_to_db():
-    conn = sqlite3.connect('user_signatures.db')
+    conn = sqlite3.connect("user_signatures.db")
     return conn
 
 
@@ -33,23 +40,27 @@ def get_file_from_db(customer_id):
 
 
 def main():
-    CREATE_TABLE = """CREATE TABLE IF NOT EXISTS signatures (customer_id TEXT PRIMARY KEY,sign1 BLOB, sign2 BLOB, sign3 BLOB)"""
+    CREATE_TABLE = """CREATE TABLE IF NOT EXISTS signatures (customer_id TEXT PRIMARY KEY,sign1 BLOB, sign2 BLOB, sign3 BLOB, sign4 BLOB)"""
     cursor = connect_to_db().cursor()
     cursor.execute(CREATE_TABLE)
     cursor.connection.commit()
     # For heroku, remove this line. We'll use gunicorn to run the app
-    app.run() # app.run(debug=True) 
+    app.run()  # app.run(debug=True)
 
-@app.route('/')
+
+@app.route("/")
 def index():
-    return render_template('index.html')
+    return render_template("index.html")
 
-@app.route('/upload', methods=['POST'])
+
+@app.route("/upload", methods=["POST"])
 def upload():
-    file1 = request.files['uploadedImage1']
-    file2 = request.files['uploadedImage2']
-    file3 = request.files['uploadedImage3']
-    customer_id = request.form['customerID']
+    file1 = request.files["uploadedImage1"]
+    file2 = request.files["uploadedImage2"]
+    file3 = request.files["uploadedImage3"]
+    file4 = request.files["uploadedImage4"]
+
+    customer_id = request.form["customerID"]
     print(customer_id)
     try:
         conn = connect_to_db()
@@ -57,26 +68,33 @@ def upload():
         query = """DELETE FROM signatures where customer_id=?"""
         cursor.execute(query, (customer_id,))
         cursor = conn.cursor()
-        query = """INSERT INTO signatures VALUES(?,?,?,?)"""
-        cursor.execute(query, (customer_id, file1.read(), file2.read(), file3.read()))
+        query = """INSERT INTO signatures VALUES(?,?,?,?,?)"""
+        cursor.execute(
+            query, (customer_id, file1.read(), file2.read(), file3.read(), file4.read())
+        )
         conn.commit()
         return jsonify({"error": False})
     except Exception as e:
         print(e)
         return jsonify({"error": True})
 
-@app.route('/verify', methods=['POST'])
+
+@app.route("/verify", methods=["POST"])
 def verify():
     try:
-        customer_id = request.form['customerID']
-        input_image = Image.open(request.files['newSignature'])
-        input_image_tensor = convert_to_image_tensor(invert_image(input_image)).view(1,1,220,155)
+        customer_id = request.form["customerID"]
+        input_image = Image.open(request.files["newSignature"])
+        input_image_tensor = convert_to_image_tensor(invert_image(input_image)).view(
+            1, 1, 220, 155
+        )
         customer_sample_images = get_file_from_db(customer_id)
         if not customer_sample_images:
-            return jsonify({'error':True})
+            return jsonify({"error": True})
         anchor_images = [Image.open(BytesIO(x)) for x in customer_sample_images]
-        anchor_image_tensors = [convert_to_image_tensor(invert_image(x)).view(-1, 1, 220, 155) 
-                        for x in anchor_images]
+        anchor_image_tensors = [
+            convert_to_image_tensor(invert_image(x)).view(-1, 1, 220, 155)
+            for x in anchor_images
+        ]
         model = load_model()
         mindist = math.inf
         for anci in anchor_image_tensors:
@@ -85,19 +103,36 @@ def verify():
             mindist = min(mindist, dist)
 
             if dist <= 0.145139:  # Threshold obtained using Test.py
-                return jsonify({"match": True, "error": False, "threshold":"%.6f" % (0.145139), "distance":"%.6f"%(mindist)})
-        return jsonify({"match": False, "error": False, "threshold":0.145139, "distance":round(mindist, 6)})
+                return jsonify(
+                    {
+                        "match": True,
+                        "error": False,
+                        "threshold": "%.6f" % (0.145139),
+                        "distance": "%.6f" % (mindist),
+                    }
+                )
+        return jsonify(
+            {
+                "match": False,
+                "error": False,
+                "threshold": 0.145139,
+                "distance": round(mindist, 6),
+            }
+        )
     except Exception as e:
         print(e)
-        return jsonify({"error":True})
+        return jsonify({"error": True})
+
 
 @app.route("/manifest.json")
 def manifest():
-    return send_from_directory('./frontend/build', 'manifest.json')
+    return send_from_directory("./frontend/build", "manifest.json")
+
 
 @app.route("/favicon.ico")
 def favicon():
-    return send_from_directory('./frontend/build', 'favicon.ico')
+    return send_from_directory("./frontend/build", "favicon.ico")
 
-if __name__=='__main__':
+
+if __name__ == "__main__":
     main()
